789a402bc24db3d117226778a2e9038c06d2ec59
samueru735 <samcontrol@hotmail.com>
2016-06-05 21:26:31 +0200

    commit

---
+++ provision_mgmt.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# install ansible (http://docs.ansible.com/intro_installation.html)
+apt-get -y install software-properties-common
+apt-add-repository -y ppa:ansible/ansible
+apt-get update
+apt-get -y install ansible
+apt-get -y install nodejs
+apt-get -y install sshpass
+
+# copy files_ansible into /home/vagrant (from inside the mgmt node)
+cp -a /vagrant/files_ansible/* /home/vagrant
+chown -R vagrant:vagrant /home/vagrant
+
+# configure hosts file for our internal network defined by Vagrantfile
+cat >> /etc/hosts <<EOL
+
+# vagrant environment nodes
+10.0.15.10  mgmt
+10.0.15.11  lb
+10.0.15.21  web1
+10.0.15.22  web2
+EOL
+
